94ae76617b8b30c45debad29bfbf128bfcb44510
woken-loves-u <105005427+woken-loves-u@users.noreply.github.com>
2022-07-03 11:56:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,7 @@ def check_all_messages(message):
     response('See you!', ['bye', 'goodbye'], single_response=True)
     response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
-    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
-
+   
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
     response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
